833adc32008b05a78880039032703f8e86bf66a7
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-01-10 23:50:43 +0100

    Combine Two Tables SQL added

--- leetcode/mysql/second-highest-salary.sql
+++ leetcode/mysql/second-highest-salary.sql
@@ -1,4 +1,14 @@
 -- https://oj.leetcode.com/problems/second-highest-salary/
 
+-- best variant
 SELECT MAX(`Salary`) FROM `Employee`
   WHERE `Salary` < (SELECT MAX(`Salary`) FROM `Employee`);
+
+-- flexible variant
+SELECT *
+  FROM (
+    SELECT `Salary` FROM `Employee`
+      WHERE `Salary` != (SELECT MAX(`Salary`) FROM `Employee`)
+  UNION
+    SELECT NULL
+  ) AS `SecondHighestSalary` ORDER BY `Salary` DESC LIMIT 1
