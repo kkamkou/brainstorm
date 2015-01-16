86d8a7d8633c888cb53db78fabd6c97456f8e4eb
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-01-16 16:19:00 +0100

    Typo

--- leetcode/mysql/employees-earning-more-than-their-managers.sql
+++ leetcode/mysql/employees-earning-more-than-their-managers.sql
@@ -2,5 +2,5 @@
 
 SELECT e1.`Name`
   FROM `Employee` e1
-  LEFT JOIN `Employee` e2 ON(e2.`Id` = e1.`ManagerId`)
-    WHERE e1.`ManagerId` IS NOT NULL AND e1.`Salary` > e2.`Salary`
+  INNER JOIN `Employee` e2 ON(e2.`Id` = e1.`ManagerId`)
+    WHERE e1.`Salary` > e2.`Salary`
