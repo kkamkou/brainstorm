737a4f9b12bf5cadec78a3fc01d3df64d6864735
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-02-06 17:27:05 +0100

    Single Number C++ added

---
+++ leetcode/cpp/single-number.cpp
@@ -0,0 +1,12 @@
+// https://oj.leetcode.com/problems/single-number/
+
+class Solution {
+public:
+    int singleNumber(int A[], int n) {
+        int r = 0;
+        for (int i = 0; i < n; i++) {
+            r ^= A[i];
+        }
+        return r;
+    }
+};
