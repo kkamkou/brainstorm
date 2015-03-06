b7b256834ab2e022a3463b6b7ea56405bf90b948
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-03-06 15:08:46 +0100

    consecutive numbers added

---
+++ leetcode/mysql/consecutive-numbers.sql
@@ -0,0 +1,12 @@
+-- https://oj.leetcode.com/problems/consecutive-numbers/
+
+SELECT DISTINCT `Num` FROM (
+  SELECT
+    `Num`,
+    CASE
+      WHEN @prevNum = `Num` THEN @count := @count + 1
+      WHEN (@prevNum := `Num`) IS NOT NULL THEN @count := 1
+    END n
+  FROM
+    `Logs`, (SELECT @prevNum := NULL) r, (SELECT @count := 1) c
+) a WHERE n >= 3
