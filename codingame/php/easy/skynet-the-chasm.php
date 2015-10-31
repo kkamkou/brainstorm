c1bc4041ba28281c57fad466e3d90fae6e2c274e
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-10-31 21:08:29 +0100

    Skynet - The Chasm, php

---
+++ codingame/php/easy/skynet-the-chasm.php
@@ -0,0 +1,28 @@
+<?php
+    fscanf(STDIN, "%d", $road);
+    fscanf(STDIN, "%d", $gap);
+    fscanf(STDIN, "%d", $platform);
+
+    while (true) {
+        fscanf(STDIN, "%d", $speed);
+        fscanf(STDIN, "%d", $coordX);
+
+        $speedRequired = $gap + 1;
+
+        if ($coordX >= $road + $gap) {
+            echo("SLOW\n");
+            continue;
+        }
+
+        if ($speed != $speedRequired) {
+            echo($speed < $speedRequired ? "SPEED\n" : "SLOW\n");
+            continue;
+        }
+
+        if ($coordX + 1 == $road) {
+            echo("JUMP\n");
+            continue;
+        }
+
+        echo("WAIT\n");
+    }
